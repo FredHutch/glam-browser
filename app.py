809f22a0a9cfa207959f9a9d6f16d331cd9e4b55
Sam Minot <sam@fredhutch.org>
2020-04-30 14:34:55 -0700

    Added PCA / t-SNE plot

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ import dash_html_components as html
 import plotly.express as px
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
+import numpy as np
 import os
 import pandas as pd
-import numpy as np
+import seaborn as sns
 
 # Read in data from the HDF5 defined in `HDF5_fp`
 hdf5_fp = os.getenv("HDF5_FP")
@@ -23,14 +24,38 @@ with pd.HDFStore(hdf5_fp, "r") as store:
     # Table for CAG SIZE GRAPH
     cag_summary_df = pd.read_hdf(store, "/annot/cag/all")
 
+    # Specimen metadata table (manifest provided by user)
+    manifest_df = pd.read_hdf(store, "/manifest")
+
+    # Precomputed PCA (on specimens)
+    pca_df = pd.read_hdf(store, "/ordination/pca")
+
+    # Precomputed t-SNE (on specimens)
+    tsne_df = pd.read_hdf(store, "/ordination/tsne")
+
 # Precompute some useful metrics
+
+# Precompute the proportion of reads which align
 richness_df = richness_df.assign(
     prop_reads_aligned=richness_df["aligned_reads"] / richness_df["n_reads"]
 )
+
+# The limits of CAG sizes
 cag_size_min = cag_summary_df["size"].min()
 cag_size_max = cag_summary_df["size"].max()
 cag_size_range = cag_size_max - cag_size_min
 cag_size_step = int(max(1, cag_size_range / 100))
+
+# Set index on manifest
+manifest_df.set_index("specimen", inplace=True)
+
+# Metadata fields found in the manifest
+metadata_fields = [n for n in manifest_df.columns.values if n not in ["R1", "R2", "I1", "I2"]]
+
+# Set index on ordination tables
+pca_df.set_index("specimen", inplace=True)
+tsne_df.set_index("specimen", inplace=True)
+
 
 # external CSS stylesheets
 external_stylesheets = [
@@ -42,6 +67,7 @@ external_stylesheets = [
     }
 ]
 
+# Set up the app
 app = dash.Dash(
     __name__, 
     external_stylesheets=external_stylesheets
@@ -170,6 +196,51 @@ app.layout = html.Div(
                 ####################
                 # / CAG SIZE GRAPH #
                 ####################
+                ####################
+                # ORDINATION GRAPH #
+                ####################
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                dcc.Graph(
+                                    id='ordination-graph'
+                                )
+                            ],
+                            className="col-sm-8",
+                        ),
+                        html.Div(
+                            [
+                                html.Label('Ordination Method'),
+                                dcc.Dropdown(
+                                    id="ordination-algorithm",
+                                    options=[
+                                        {'label': 'PCA', 'value': 'pca'},
+                                        {'label': 't-SNE', 'value': 'tsne'},
+                                    ],
+                                    value='pca'
+                                ),
+                                html.Br(),
+                                html.Label('Metadata Label'),
+                                dcc.Dropdown(
+                                    id="ordination-metadata",
+                                    options=[
+                                        {'label': 'None', 'value': 'none'},
+                                    ] + [
+                                        {'label': f, "value": f}
+                                        for f in metadata_fields
+                                    ],
+                                    value='none'
+                                ),
+                            ],
+                            className="col-sm-4",
+                        )
+                    ],
+                    className="row"
+                ),
+                ######################
+                # / ORDINATION GRAPH #
+                ######################
             ],
             className="container"
         )
@@ -281,6 +352,85 @@ def draw_cag_size(selected_range, log_scale):
         },
         xaxis_title="CAG Size ({}number of genes per CAG)".format("log10 " if log_scale == "on" else ""), 
         yaxis_title="Number of Genes (per bin)", 
+        template="simple_white"
+    )
+
+    return fig
+
+
+####################
+# ORDINATION GRAPH #
+####################
+@app.callback(
+    Output('ordination-graph', 'figure'),
+    [
+        Input('ordination-algorithm', 'value'),
+        Input('ordination-metadata', 'value'),
+    ])
+def draw_ordination(algorithm, metadata):
+
+    if algorithm == "pca":
+        plot_df = pca_df
+    else:
+        assert algorithm == "tsne"
+        plot_df = tsne_df
+
+    if metadata == "none":
+
+        fig = go.Figure(
+            data=go.Scatter(
+                x=plot_df[plot_df.columns.values[0]],
+                y=plot_df[plot_df.columns.values[1]],
+                ids=plot_df.index.values,
+                text=plot_df.index.values,
+                hoverinfo="text",
+                mode="markers",
+            ),
+        )
+
+    else:
+
+        plot_df[metadata] = manifest_df[metadata]
+
+        cmap = dict(zip(
+            plot_df[metadata].unique(),
+            sns.color_palette(
+                "colorblind",
+                plot_df[metadata].unique().shape[0],
+            ).as_hex()
+        ))
+
+        fig = go.Figure()
+
+        for group_name, group_plot_df in plot_df.groupby(metadata):
+            fig.add_trace(
+                go.Scatter(
+                    x=group_plot_df[plot_df.columns.values[0]],
+                    y=group_plot_df[plot_df.columns.values[1]],
+                    ids=group_plot_df.index.values,
+                    text=group_plot_df.index.values,
+                    hoverinfo="text",
+                    mode="markers",
+                    marker={
+                        "color": cmap[group_name]
+                    },
+                    legendgroup=metadata,
+                    name=group_name,
+                ),
+            )
+
+        fig.update_layout(showlegend=True)
+
+    fig.update_layout(
+        title={
+            'text': "PCA" if algorithm == "pca" else "t-SNE",
+            'y': 0.9,
+            'x': 0.5, 
+            'xanchor': 'center',
+            'yanchor': 'top',
+        },
+        xaxis_title=plot_df.columns.values[0],
+        yaxis_title=plot_df.columns.values[1],
         template="simple_white"
     )
 
