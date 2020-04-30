8fe7df52354a866f26398ed14c2c45722a3fb4b1
Sam Minot <sam@fredhutch.org>
2020-04-30 13:58:38 -0700

    Added CAG size histogram

--- app.py
+++ app.py
@@ -10,17 +10,27 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 import os
 import pandas as pd
+import numpy as np
 
 # Read in data from the HDF5 defined in `HDF5_fp`
 hdf5_fp = os.getenv("HDF5_FP")
 assert os.path.exists(hdf5_fp), "Path does not exist: {}".format(hdf5_fp)
 with pd.HDFStore(hdf5_fp, "r") as store:
+
+    # Table for RICHNESS GRAPH
     richness_df = pd.read_hdf(store, "/summary/all")
+
+    # Table for CAG SIZE GRAPH
+    cag_summary_df = pd.read_hdf(store, "/annot/cag/all")
 
 # Precompute some useful metrics
 richness_df = richness_df.assign(
     prop_reads_aligned=richness_df["aligned_reads"] / richness_df["n_reads"]
 )
+cag_size_min = cag_summary_df["size"].min()
+cag_size_max = cag_summary_df["size"].max()
+cag_size_range = cag_size_max - cag_size_min
+cag_size_step = int(max(1, cag_size_range / 100))
 
 # external CSS stylesheets
 external_stylesheets = [
@@ -107,7 +117,59 @@ app.layout = html.Div(
                         )
                     ],
                     className="row"
-                )
+                ),
+                ####################
+                # / RICHNESS GRAPH #
+                ####################
+                ##################
+                # CAG SIZE GRAPH #
+                ##################
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                dcc.Graph(
+                                    id='cag-size-graph'
+                                )
+                            ],
+                            className="col-sm-8",
+                        ),
+                        html.Div(
+                            [
+                                html.Label('CAG Size Window'),
+                                dcc.RangeSlider(
+                                    id="cag-size-slider",
+                                    min=cag_size_min,
+                                    max=cag_size_max,
+                                    step=cag_size_step,
+                                    marks={
+                                        str(n): str(n)
+                                        for n in np.arange(cag_size_min, cag_size_max, max(1, int(cag_size_range / 5)))
+                                    },
+                                    value=[
+                                        cag_size_min,
+                                        cag_size_max
+                                    ]
+                                ),
+                                html.Br(),
+                                html.Label('Log Scale'),
+                                dcc.RadioItems(
+                                    id="cag-size-log",
+                                    options=[
+                                        {'label': 'On', 'value': 'on'},
+                                        {'label': 'Off', 'value': 'off'},
+                                    ],
+                                    value='off',
+                                )
+                            ],
+                            className="col-sm-4",
+                        )
+                    ],
+                    className="row"
+                ),
+                ####################
+                # / CAG SIZE GRAPH #
+                ####################
             ],
             className="container"
         )
@@ -121,6 +183,10 @@ layout = go.Layout(
 )
 
 # Functions used to render graphs
+
+##################
+# RICHNESS GRAPH #
+##################
 @app.callback(
     Output('richness-graph', 'figure'),
     [
@@ -170,6 +236,51 @@ def draw_richness(selected_metric, selected_type):
             "n_genes_aligned": "Num. Genes Aligned",
             "n_genes_assembled": "Num. Genes Assembled",
         }[selected_metric], 
+        template="simple_white"
+    )
+
+    return fig
+
+
+##################
+# CAG SIZE GRAPH #
+##################
+@app.callback(
+    Output('cag-size-graph', 'figure'),
+    [
+        Input('cag-size-slider', 'value'),
+        Input('cag-size-log', 'value'),
+    ])
+def draw_cag_size(selected_range, log_scale):
+
+    plot_vals = cag_summary_df.query(
+        "size >= %s" % selected_range[0]
+    ).query(
+        "size <= %s" % selected_range[1]
+    )["size"]
+
+    if log_scale == "on":
+        plot_vals = plot_vals.apply(np.log10)
+
+    fig = go.Figure(
+        data=[
+            go.Histogram(
+                x=plot_vals,
+                histfunc="sum"
+            )
+        ],
+    )
+
+    fig.update_layout(
+        title={
+            'text': "CAG Size",
+            'y': 0.9,
+            'x': 0.5, 
+            'xanchor': 'center',
+            'yanchor': 'top'
+        },
+        xaxis_title="CAG Size ({}number of genes per CAG)".format("log10 " if log_scale == "on" else ""), 
+        yaxis_title="Number of Genes (per bin)", 
         template="simple_white"
     )
 
