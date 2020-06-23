663522b687fc395f9d100d2015f5f6b840fc8cb8
Sam Minot <sam@fredhutch.org>
2020-06-23 08:44:14 -0700

    Increase timeout for local execution

--- run_local.sh
+++ run_local.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-DATA_FOLDER=data gunicorn --workers 4 --worker-class gevent --bind 0.0.0.0:8050 app:server
+DATA_FOLDER=data gunicorn --workers 4 --worker-class gevent --bind 0.0.0.0:8050 --timeout 120 app:server
